7c0445c5951efae1b71174a669114864262eb22e
Dylan Thomson <djthomson88@gmail.com>
2017-06-08 20:41:59 -0400

    Fix weekly multiplier
    -1 year is 52.1429 weeks, not 56 weeks...

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $('form').submit(function () {
 	
  var freqMultiplier;
  if(freq === "daily") freqMultiplier = 365.25;
- else if(freq === "weekly") freqMultiplier = 56;
+ else if(freq === "weekly") freqMultiplier = 52.1429;
  else if(freq === "monthly") freqMultiplier = 12;
  else if(freq === "yearly") freqMultiplier = 1;
 
