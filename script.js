96f964e2a03f006930c012c754fdd7aff734be06
Dylan Thomson <djthomson88@gmail.com>
2017-06-08 20:20:05 -0400

    Calculate yearly cost
    -Remove "for so many days/weeks/months" for now
    -Default calculation is "per year"
    -Calculate frequency modifier based on option selected
    -Calculate total
    -Hide output until submit is pressed
    -Update output to display total spent yearly

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+
+
+
 $('form').submit(function () {
  var spent = $('#inputSpent').val();
  var times = $('#inputTimes').val();
  var freq = $('#inputFrequency').val();
- var durationNum = $('#inputDurationNum').val();
- var duration = $('#inputDuration').val();
+ // var durationNum = $('#inputDurationNum').val();
+ // var duration = $('#inputDuration').val();
+	
+ var freqMultiplier;
+ if(freq === "daily") freqMultiplier = 365.25;
+ else if(freq === "weekly") freqMultiplier = 56;
+ else if(freq === "monthly") freqMultiplier = 12;
+ else if(freq === "yearly") freqMultiplier = 1;
 
- alert("You spend $" + spent + " " + times + " times " + freq + " for " + durationNum + " " + duration);
+ var total = spent * times * freqMultiplier;
+
+ 	$('#outputSpent').text(total);
+ 	$('#output').removeClass("hidden");
  return false;
 });
